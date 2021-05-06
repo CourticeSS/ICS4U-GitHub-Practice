c1a398c2f0ad63689115459ed98bad8b777ca061
mark_watson <mrwatsonsst@users.noreply.github.com>
2021-05-06 00:33:02 -0400

    Update change_me.py
    Trying something here

--- change_me.py
+++ change_me.py
@@ -7,10 +7,11 @@
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+    print("How are you?")
 
 
 def print_best_team(team):
-    print("Maple Leafs)
+    print(f"The best team is the {team}.")
 
 
 
@@ -20,6 +21,6 @@ def print_best_team(team):
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('PyCharm')
-    print_best_team("Maple Leafs")
+    print_best_team("Toronto Maple Leafs")
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
